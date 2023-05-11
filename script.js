da890668651ed10fed21f7059f55edddc4e73a5e
Moiz Ahmed <moizahmedbss786@gmail.com>
2023-05-11 05:04:38 +0500

    Just a funky calculator

--- script.js
+++ script.js
@@ -1,29 +1,130 @@
 
 let resultInput = document.getElementById("result");
-function appendValue(value) {
+let operatorUsed = false;
+function rad()
+{
+    let radians = parseFloat(resultInput.value);
+    resultInput.value = radians * Math.PI / 180;
+}
+function deg()
+{
+    let degrees = parseFloat(resultInput.value);
+    resultInput.value = degrees * (180 / Math.PI);
+}
+function percent()
+{
+    resultInput.value = parseFloat(resultInput.value)/100;
+}
+function answer()
+{
+    resultInput.value = "Answer = " + resultInput.value;
+}
+function expo()
+{
+    resultInput.value+='E';
+}
+function factorial()
+{
+    try {
+        let number = parseInt(resultInput.value);
+        let longNumber = 1;
+        for (let i = 1; i <= number; i++)
+        {
+            longNumber*=i;
+        }
+        resultInput.value = longNumber;
+    }
+    catch (error)
+    {
+        resultInput.value = "Math Error"
+    }
+}
+function operation(operator) {
+    operatorUsed = true;
+    resultInput.value += operator;
+}
+function pi()
+{
+    if(!operatorUsed)
+    {
+        resultInput.value+="*";
+    }
+    resultInput.value+=Math.PI;
+}
+function euler()
+{
+    if(!operatorUsed)
+    {
+        resultInput.value+="*";
+    }
+    resultInput.value+=Math.E;
+}
+function Inverse()
+{
+    resultInput.value = 1/parseFloat(resultInput.value);
+}
+function Log(log)
+{
+    if(log==='ln')
+    {
+        if(resultInput.value.trim()==='')
+        {
+            resultInput.value = "Please enter a number";
+        }
+        else {
+            let number = parseFloat(resultInput.value);
+            resultInput.value = Math.log(number);
+        }
+    }
+    else
+    {
+        if(resultInput.value.trim()==='')
+        {
+            resultInput.value = "Please enter a number";
+        }
+        else {
+            let number = parseFloat(resultInput.value);
+            resultInput.value = Math.log10(number);
+        }
+    }
+}
+function power(pow)
+{
+    if(pow==='sqrt')
+    {
+        let number = parseFloat(resultInput.value);
+        resultInput.value = Math.sqrt(number);
+    }
+    else
+    {
+        resultInput.value+='**';
+    }
+}
+function number(value) {
+    operatorUsed = false;
     resultInput.value += value;
 }
-function appendOperator(operator) {
-    if (operator === 'Sin'){
+
+function Trig(operator)
+{
+    if (operator === 'sin'){
         let angle = parseFloat(resultInput.value);
         resultInput.value = Math.sin(angle);
     }
-    else if(operator === 'Cos'){
+    else if(operator === 'cos'){
         let angle = parseFloat(resultInput.value);
         resultInput.value = Math.cos(angle);
     }
-    else if(operator==='Tan'){
+    else if(operator==='tan'){
         let angle = parseFloat(resultInput.value);
         resultInput.value = Math.tan(angle);
     }
-    else {
-        resultInput.value += operator;
-    }
 }
-function clearResult() {
+function cls() {
+    operatorUsed = false;
     resultInput.value = "";
 }
-function calculate(operator,value) {
+function equal() {
     try {
         resultInput.value = eval(resultInput.value);
     } catch (error) {
